62e646368aa267cddd03f82fc9ce082c30651853
DaveMukund <chumundas@gmail.com>
2026-04-08 14:25:47 +0530

    Add files via upload

---
+++ joblib1.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load("student_model.pkl")
+
+st.title("Student Pass/Fail Predictor")
+
+st.write("Enter student details")
+
+study_hours = st.number_input("Study Hours",0,12)
+
+
+if st.button("predict"):
+    input_data = np.array([[study_hours]])
+    prediction = model.predict(input_data)
+    
+    
+    st.success(prediction)
+      
